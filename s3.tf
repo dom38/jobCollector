5c3ff17e403317956a9125ffe8ddbcefea4917c1
Dominic Byrne <dominic@ecs-digital.co.uk>
2018-12-18 13:46:52 +0000

    uncommented s3 stuff

--- s3.tf
+++ s3.tf
@@ -1,15 +1,15 @@
 
 
-# resource "aws_s3_bucket_object" "private" {
-#   bucket = "${var.private_s3_bucket}"
-#   key    = "index.html"
-#   source = "./private_site.html"
-#   etag   = "${md5(file("./private_site.html"))}"
-# }
+resource "aws_s3_bucket_object" "private" {
+  bucket = "${var.private_s3_bucket}"
+  key    = "index.html"
+  source = "./private_site.html"
+  etag   = "${md5(file("./private_site.html"))}"
+}
 
-# resource "aws_s3_bucket_object" "public" {
-#   bucket = "${var.public_s3_bucket}"
-#   key    = "index.html"
-#   source = "./public_site.html"
-#   etag   = "${md5(file("./public_site.html"))}"
-# }
+resource "aws_s3_bucket_object" "public" {
+  bucket = "${var.public_s3_bucket}"
+  key    = "index.html"
+  source = "./public_site.html"
+  etag   = "${md5(file("./public_site.html"))}"
+}
