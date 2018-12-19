26a2a9049d7be536362afad228fae8ea82742a1b
Dominic Byrne <dominic@ecs-digital.co.uk>
2018-12-19 10:03:34 +0000

    added random bucket and output

--- s3.tf
+++ s3.tf
@@ -1,15 +1,31 @@
 
+resource "aws_s3_bucket" "website" {
+
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "form.html"
+
+  }
+}
 
 resource "aws_s3_bucket_object" "private" {
-  bucket = "${var.private_s3_bucket}"
+  bucket = "${aws_s3_bucket.website.id}"
   key    = "index.html"
+  content_type = "text/html"
   source = "./private_site.html"
-  etag   = "${md5(file("./private_site.html"))}"
+  acl = "public-read"
 }
 
 resource "aws_s3_bucket_object" "public" {
-  bucket = "${var.public_s3_bucket}"
-  key    = "index.html"
+  bucket = "${aws_s3_bucket.website.id}"
+  key    = "form.html"
+  content_type = "text/html"
   source = "./public_site.html"
-  etag   = "${md5(file("./public_site.html"))}"
+  acl = "public-read"
+}
+
+output "Endpoint" {
+  value = "${aws_s3_bucket.website.website_endpoint}"
 }
